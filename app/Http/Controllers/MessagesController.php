b6bcdb58afd587f638cd4f1cc9474b24fba3b830
Affan Ali <affanali577@gmail.com>
2022-04-07 15:23:18 +0500

    inbox message setup and model relations setup

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $messages = Message::with('userFrom')->where('to_user_id', auth()->user()->id)->get();
+        // dd($messages);
+        return view('home' , compact('messages'));
+    }
+
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    public function userFrom()
+    {
+        return $this->belongsTo('App\Models\User', 'from_user_id');
+    }
 }
